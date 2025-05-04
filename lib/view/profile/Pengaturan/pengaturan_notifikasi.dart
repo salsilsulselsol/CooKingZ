eb21ea12b62bd471476f90423d0c76deae5f8525
yazidmadarizel <69192961+yazidmadarizel@users.noreply.github.com>
2025-05-04 22:27:34 +0700

    Update pengaturan_notifikasi.dart

--- lib/view/profile/Pengaturan/pengaturan_notifikasi.dart
+++ lib/view/profile/Pengaturan/pengaturan_notifikasi.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../component/custom_appbar.dart';
+import '../../component/custom_switch.dart';
+
 class NotificationSettingsScreen extends StatefulWidget {
   const NotificationSettingsScreen({Key? key}) : super(key: key);
 
@@ -17,44 +20,15 @@ class _NotificationSettingsScreenState extends State<NotificationSettingsScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-       leading: GestureDetector(
-          onTap: () => Navigator.pushNamed(context, "/home"),
-          child: Transform.translate(
-            offset: const Offset(15, 0), // Geser tombol 15px ke kanan
-            child: SizedBox(
-              width: 30, // Area klik lebih besar dari gambar
-              height: 30,
-              child: Center(
-                child: SizedBox(
-                  width: 24,
-                  height: 24,
-                  child: Image.asset(
-                    'images/Tombol_kembali.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        title: Text(
-          'Notifikasi',
-          style: TextStyle(
-            color: Color(0xFF006A4E),
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
+      appBar: CustomAppBar(
+        title: 'Notifikasi',
+        onBackPressed: () => Navigator.pushNamed(context, "/home"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
         child: Column(
           children: [
-            _buildSwitchRow(
+            SwitchSettingRow(
               title: 'Notifikasi Umum',
               value: generalNotifications,
               onChanged: (value) {
@@ -63,8 +37,8 @@ class _NotificationSettingsScreenState extends State<NotificationSettingsScreen>
                 });
               },
             ),
-            _buildDivider(),
-            _buildSwitchRow(
+            CustomDivider(),
+            SwitchSettingRow(
               title: 'Suara',
               value: soundNotifications,
               onChanged: (value) {
@@ -73,8 +47,8 @@ class _NotificationSettingsScreenState extends State<NotificationSettingsScreen>
                 });
               },
             ),
-            _buildDivider(),
-            _buildSwitchRow(
+            CustomDivider(),
+            SwitchSettingRow(
               title: 'Getar',
               value: vibrationNotifications,
               onChanged: (value) {
@@ -83,44 +57,19 @@ class _NotificationSettingsScreenState extends State<NotificationSettingsScreen>
                 });
               },
             ),
-            _buildDivider(),
+            CustomDivider(),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget _buildSwitchRow({
-    required String title,
-    required bool value,
-    required ValueChanged<bool> onChanged,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Switch(
-            value: value,
-            onChanged: onChanged,
-            activeColor: Colors.white,
-            activeTrackColor: Color(0xFF006A4E),
-            inactiveThumbColor: Colors.white,
-            inactiveTrackColor: Colors.grey.shade300,
-          ),
-        ],
-      ),
-    );
-  }
 
-  Widget _buildDivider() {
+
+class CustomDivider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Divider(
       height: 1,
       thickness: 0.5,
