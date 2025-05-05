74f34a11bff0095f5a56cf5f47c3207036cc84ee
MFarhan21004 <mfarhan2104@upi.edu>
2025-05-05 15:30:12 +0700

    done

--- lib/view/profile/profil/makanan_favorit.dart
+++ lib/view/profile/profil/makanan_favorit.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:masak2/models/food_model.dart';
+import '../../component/grid_2_builder.dart';
 
 class MakananFavorit extends StatelessWidget {
   const MakananFavorit({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Food> recipes = [
+      Food(
+        name: 'French Toast',
+        description: 'Rican ini yang klasik',
+        image: 'images/french_toast.jpg',
+        cookingTime: 25,
+        price: '35.000',
+        likes: 30,
+      ),
+      Food(
+        name: 'Crepes Buah',
+        description: 'Crepes cream isi buah',
+        image: 'images/crepes.png',
+        cookingTime: 15,
+        price: '30.000',
+        likes: 27,
+      ),
+      Food(
+        name: 'Macarons',
+        description: 'Klasik, berwarna-warni, dan lembut manis',
+        image: 'images/macarons.jpg',
+        cookingTime: 60,
+        price: '40.000',
+        likes: 38,
+      ),
+      Food(
+        name: 'Spring Cupcake',
+        description: 'Cupcake di spring, bertopping kurma mudah siap',
+        image: 'images/springcake.png',
+        cookingTime: 45,
+        price: '28.000',
+        likes: 25,
+      ),
+      Food(
+        name: 'Cheesecake',
+        description: 'Cheesecake dingin yang lemon dan lembut',
+        image: 'images/chesscake.png',
+        cookingTime: 50,
+        price: '45.000',
+        likes: 32,
+      ),
+      Food(
+        name: 'Es Kopi',
+        description: 'Kopi susu dingin dan segar',
+        image: 'images/eskopi.png',
+        cookingTime: 10,
+        price: '25.000',
+        likes: 29,
+      ),
+    ];
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -12,18 +65,18 @@ class MakananFavorit extends StatelessWidget {
         elevation: 0,
         centerTitle: true,
         leading: GestureDetector(
-          onTap: () => Navigator.pushNamed(context, "/home"),
+          onTap: () => Navigator.pushNamed(context, "/"),
           child: Transform.translate(
-            offset: const Offset(15, 0), // Geser tombol 15px ke kanan
+            offset: const Offset(15, 0),
             child: SizedBox(
-              width: 30, // Area klik lebih besar dari gambar
+              width: 30,
               height: 30,
               child: Center(
                 child: SizedBox(
                   width: 24,
                   height: 24,
                   child: Image.asset(
-                    'images/Tombol_kembali.png',
+                    'images/arrow.png',
                     fit: BoxFit.contain,
                   ),
                 ),
@@ -31,7 +84,7 @@ class MakananFavorit extends StatelessWidget {
             ),
           ),
         ),
-        title: Text(
+        title: const Text(
           'Manis',
           style: TextStyle(
             color: Color(0xFF006A4E),
@@ -40,217 +93,20 @@ class MakananFavorit extends StatelessWidget {
           ),
         ),
       ),
-      body: _buildRecipeGrid(),
-    );
-  }
-
-  Widget _buildRecipeGrid() {
-    // List of recipes
-    List<Map<String, dynamic>> recipes = [
-      {
-        'name': 'French Toast',
-        'description': 'Rican ini yang klasik',
-        'image': 'images/french_toast.jpg',
-        'duration': '25 menit',
-        'price': '35RB',
-        'likes': 30,
-      },
-      {
-        'name': 'Crepes Buah',
-        'description': 'Crepes cream isi buah',
-        'image': 'images/manis.png',
-        'duration': '15 menit',
-        'price': '30RB',
-        'likes': 27,
-      },
-      {
-        'name': 'Macarons',
-        'description': 'Klasik, berwarna-warni, dan lembut manis',
-        'image': 'images/macarons.jpg',
-        'duration': '60 menit',
-        'price': '40RB',
-        'likes': 38,
-      },
-      {
-        'name': 'Spring Cupcake',
-        'description': 'Cupcake di spring, bertopping kurma mudah siap',
-        'image': 'images/springcake.png',
-        'duration': '45 menit',
-        'price': '28RB',
-        'likes': 25,
-      },
-      {
-        'name': 'Cheesecake',
-        'description': 'Cheesecake dingin yang lemon dan lembut',
-        'image': 'images/chesscake.png', 
-        'duration': '50 menit',
-        'price': '45RB',
-        'likes': 32,
-      },
-      {
-        'name': 'Es Kopi',
-        'description': 'Kopi susu dingin dan segar',
-        'image': '/images/eskopi.ong',
-        'duration': '10 menit',
-        'price': '25RB',
-        'likes': 29,
-      },
-    ];
-
-    return GridView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 16),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 12,
-        childAspectRatio: 0.8,
-      ),
-      itemCount: recipes.length,
-      itemBuilder: (context, index) {
-        return _buildFoodCard(recipes[index]);
-      },
-    );
-  }
-
-  Widget _buildFoodCard(Map<String, dynamic> food) {
-    return Card(
-      elevation: 0,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Image container with heart icon
-          Stack(
-            children: [
-              // Food image
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(12),
-                  topRight: Radius.circular(12),
-                ),
-                child: Image.asset(
-                  food['image'],
-                  height: 120,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              // Heart icon button
-              Positioned(
-                top: 8,
-                right: 8,
-                child: Container(
-                  width: 28,
-                  height: 28,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF57B4BA),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.favorite_border,
-                    color: Colors.white,
-                    size: 16,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          // Card content
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Color(0xFFE0E0E0),
-                width: 1,
-              ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(12),
-                bottomRight: Radius.circular(12),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Food name
-                Text(
-                  food['name'],
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                    color: Color(0xFF006A4E),
-                  ),
-                ),
-                SizedBox(height: 4),
-                // Description
-                Text(
-                  food['description'],
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey[600],
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 8),
-                // Bottom row with icons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Likes
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: Color(0xFF57B4BA),
-                          size: 14,
-                        ),
-                        SizedBox(width: 4),
-                        Text(
-                          food['likes'].toString(),
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xFF57B4BA),
-                          ),
-                        ),
-                      ],
-                    ),
-                    // Duration
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.access_time,
-                          color: Color(0xFF57B4BA),
-                          size: 14,
-                        ),
-                        SizedBox(width: 4),
-                        Text(
-                          food['duration'],
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Color(0xFF57B4BA),
-                          ),
-                        ),
-                      ],
-                    ),
-                    // Price
-                    Text(
-                      "Rp${food['price']}",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF57B4BA),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: FoodGridWidget(
+        foods: recipes,
+        onFavoritePressed: (index) {
+          final food = recipes[index];
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Favorited ${food.name}')),
+          );
+        },
+        onCardTap: (index) {
+          final food = recipes[index];
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Tapped on ${food.name}')),
+          );
+        },
       ),
     );
   }
